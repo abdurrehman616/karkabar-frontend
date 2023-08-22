a9cee3341f192c9eb8a379265f82b65754c88fe3
abdurrehman616 <abdurrehman6869@gmail.com>
2023-08-22 23:50:59 +0500

    feat: add ProductCard

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,18 @@
+const ProductCard = () => {
+    return (
+      <div className="flex w-full">
+        {/*  Image  */}
+          <div className="flex w-full">
+              <img src="https://red-parts.html.themeforest.scompiler.ru/themes/blue-ltr/images/products/product-1-245x245.jpg"/>
+          </div>
+        {/*  SKU  */}
+            <span>SKU:  473-75662-R</span>
+        {/*  Product Title  */}
+          <span>Brandix Spark Plug Kit ASR-400</span>
+        {/*  Price and Add To Cart  */}
+          <></>
+      </div>
+    )
+}
+
+export default ProductCard
