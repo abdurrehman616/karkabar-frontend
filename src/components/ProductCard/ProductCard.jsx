5d5fc003452ababb7de4bc816613a298df0bf69d
abdurrehman616 <abdurrehman6869@gmail.com>
2023-08-24 23:29:25 +0500

    feat

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,16 +1,19 @@
 const ProductCard = () => {
     return (
-      <div className="flex w-full">
+      <div className="small-container flex flex-col w-1/6 shadow-lg">
         {/*  Image  */}
           <div className="flex w-full">
               <img src="https://red-parts.html.themeforest.scompiler.ru/themes/blue-ltr/images/products/product-1-245x245.jpg"/>
           </div>
         {/*  SKU  */}
-            <span>SKU:  473-75662-R</span>
+            <span className="text-xs text-gray-400">SKU: 473-75662-R</span>
         {/*  Product Title  */}
           <span>Brandix Spark Plug Kit ASR-400</span>
         {/*  Price and Add To Cart  */}
-          <></>
+          <div className="flex justify-between items-center pt-10">
+              <span className="text-lg font-semibold">$ 50.0</span>
+              <i className="far fa-shopping-cart"/>
+          </div>
       </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from "../components/Banner/Banner.jsx";
 import FeatureList from "../components/FeatureList/FeatureList.jsx";
+import ProductCard from "../components/ProductCard/ProductCard.jsx";
 
 const Home = () => {
     return (
@@ -9,8 +10,53 @@ const Home = () => {
             
             {/*  Feature List  */}
             <FeatureList />
-            
+    
+            <div className="divider px-20 m-0"></div>
+    
+    
             {/*  Featured Products   */}
+            <div className="container flex flex-col">
+                {/*title*/}
+                <div className="flex justify-between items-center px-12">
+                    <span className="text-xl font-bold">Feature Products</span>
+                    
+                    <div className="flex gap-8 items-center">
+                        {/*  tags   */}
+                        <div className="background-gray px-3 py-1 skew-x-[-20deg]">
+                            <button className="text-white text-sm skew-x-[20deg]">All</button>
+                        </div>
+                        <div className="flex">
+                            <button className="text-gray-400 text-sm">Power Tools</button>
+                        </div>
+                        <div className="flex">
+                            <button className="text-gray-400 text-sm">Hand Tools</button>
+                        </div>
+                        <div className="flex">
+                            <button className="text-gray-400 text-sm">Plumbing</button>
+                        </div>
+                        
+                        {/*  buttons  */}
+                        <div className="flex gap-1">
+                            <div className="bg-blue-400 px-2 py-1 -skew-x-[20deg]">
+                                <button className="text-white skew-x-[20deg]">&lt;</button>
+                            </div>
+                            <div className="bg-blue-400 px-2 py-1 -skew-x-[20deg]">
+                                <button className="text-white skew-x-[20deg]">&gt;</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="divider px-12 m-0"></div>
+                
+                <div className="flex justify-center gap-10">
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                </div>
+            </div>
         </>
     );
 }
